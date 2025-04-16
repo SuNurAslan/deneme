93076d8f05d7f16d7a703d713cbaa6411a53be15
Su Nur ASLAN <sunurarslan@gmail.com>
2025-04-16 13:00:12 +0300

    sayılar

---
+++ birkiuc.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yeniuygula;
+
+
+public class birkiuc {
+    public static void main(String[] args) {
+        
+        for (int satir = 1; satir < 5; satir ++) {
+            for (int sayi = 1; sayi < satir; sayi++) {
+                System.out.print(sayi);
+            }
+            System.out.println();
+        }
+        String Kullaniciadi="ACELİK";
+        String Sifre ="567";
+        
+                
+    }
+    
+}
