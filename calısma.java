35f0ce0a8edae66a12a750ed369d36360d1cffdd
Su Nur ASLAN <sunurarslan@gmail.com>
2025-04-21 11:26:51 +0300

    kucukten büyüğe

--- calısma.java
+++ calısma.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package yeniuygula;
 
-/**
- *
- * @author VICTUS
- */
+import java.util.Scanner;
+
+
 public class calısma {
+    public static void main(String[] args) {
+        
     
+     Scanner input = new Scanner(System.in);
+        System.out.println("Bir a sayı giriniz:");
+     int a = input.nextInt();
+        System.out.println("Bir b sayısını giriniz: ");
+        int b = input.nextInt();
+        System.out.println("bir kelime giriniz:");
+        String str= input.next();
+        System.out.println("a sayısı :" + a );
+        System.out.println("b sayısı :" + b);
+        
+}
 }
