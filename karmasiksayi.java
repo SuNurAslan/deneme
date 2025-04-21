ed4e4751b69fba453963e93999586ece66aab0b0
Su Nur ASLAN <sunurarslan@gmail.com>
2025-04-21 11:53:14 +0300

    sözlük sıralaması dizi örneği

--- karmasiksayi.java
+++ karmasiksayi.java
@@ -9,6 +9,35 @@ package yeniuygula;
  *
  * @author VICTUS
  */
-public class karmasiksayi {
-    
+
+    // iki karmaşık sayının toplamını yapan motot
+
+//İKİ MATRİSİN TOPLAMI!!!!
+class karmasık {
+    public int reel;
+    public int imajiner;
 }
+public class karmasiksayi {
+public static karmasık topla (karmasık a ,karmasık b){
+    karmasık sonuc = new karmasık();
+    sonuc.reel= a.reel+b.reel;
+    sonuc.imajiner=a.imajiner+b.imajiner;
+    return sonuc;
+}
+    public static void main(String[] args) {
+        karmasık a = new karmasık();
+        karmasık b = new karmasık();
+        a.reel=10;
+        a.imajiner=100;
+        b.reel=30;
+        b.imajiner=50;
+        
+        karmasık değer = new karmasık();
+        değer=topla(a, b);
+        System.out.println(değer.reel+"+" +değer.imajiner+"i");
+    }
+
+}
+
+    
+
