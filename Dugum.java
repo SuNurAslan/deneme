1ddf197d9a24c64f228e76184baa58b50eb6dc0a
Su Nur ASLAN <sunurarslan@gmail.com>
2025-04-22 12:16:32 +0300

    düğüm içeriği classı

---
+++ Dugum.java
@@ -0,0 +1,19 @@
+
+package javaapplication2;
+
+
+public class Dugum {
+    
+    int icerik;
+    Dugum sol, sag,ileri;
+
+    public Dugum(int icerik) {
+        this.icerik= icerik;
+        sol=null;
+        sag=null;
+        ileri=null;
+    }
+  
+    
+}
+
